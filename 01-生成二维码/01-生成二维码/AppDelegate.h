91adac4469b07bd4598e725d8c635ad077465bbc
范银龙 <fanyinlong@CDMA.local>
2018-11-19 18:06:28 +0800

    初始化项目demo 2018.11.19

---
+++ 01-生成二维码/01-生成二维码/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  01-生成二维码
+//
+//  Created by xiaomage on 15/12/25.
+//  Copyright © 2015年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 01-生成二维码/01-生成二维码/ViewController.m
@@ -0,0 +1,72 @@
+//
+//  ViewController.m
+//  01-生成二维码
+//
+//  Created by xiaomage on 15/12/25.
+//  Copyright © 2015年 xiaomage. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <CoreImage/CoreImage.h>
+#import "CIImage+Extension.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    // 1.创建滤镜对象
+    CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+    
+    // 2.恢复默认设置
+    [filter setDefaults];
+    
+    // 3.设置数据
+    NSString *info = @"http://www.520it.com";
+    NSData *infoData = [info dataUsingEncoding:NSUTF8StringEncoding];
+    [filter setValue:infoData forKeyPath:@"inputMessage"];
+    
+    // 4.生成二维码
+    CIImage *outputImage = [filter outputImage];
+    self.imageView.image = [outputImage createNonInterpolatedWithSize:50];
+}
+
+/**
+ *  根据CIImage生成指定大小的UIImage
+ *
+ *  @param image CIImage
+ *  @param size  图片宽度
+ */
+//- (UIImage *)createNonInterpolatedUIImageFormCIImage:(CIImage *)image withSize:(CGFloat) size
+//{
+//    CGRect extent = CGRectIntegral(image.extent);
+//    CGFloat scale = MIN(size/CGRectGetWidth(extent), size/CGRectGetHeight(extent));
+//    
+//    // 1.创建bitmap;
+//    size_t width = CGRectGetWidth(extent) * scale;
+//    size_t height = CGRectGetHeight(extent) * scale;
+//    CGColorSpaceRef cs = CGColorSpaceCreateDeviceGray();
+//    CGContextRef bitmapRef = CGBitmapContextCreate(nil, width, height, 8, 0, cs, (CGBitmapInfo)kCGImageAlphaNone);
+//    CIContext *context = [CIContext contextWithOptions:nil];
+//    CGImageRef bitmapImage = [context createCGImage:image fromRect:extent];
+//    CGContextSetInterpolationQuality(bitmapRef, kCGInterpolationNone);
+//    CGContextScaleCTM(bitmapRef, scale, scale);
+//    CGContextDrawImage(bitmapRef, extent, bitmapImage);
+//    
+//    // 2.保存bitmap到图片
+//    CGImageRef scaledImage = CGBitmapContextCreateImage(bitmapRef);
+//    CGContextRelease(bitmapRef);
+//    CGImageRelease(bitmapImage);
+//    return [UIImage imageWithCGImage:scaledImage];
+//}
+
+@end
